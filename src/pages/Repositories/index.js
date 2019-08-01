7e67102caa568b895d73867e885a5db7d279df93
andersonfrfilho <andersonfrfilho@gmail.com>
2019-08-01 11:52:41 -0300

    Adicionando Babel

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,29 +1,39 @@
 import React, { Component } from "react";
 
-import { ActivityIndicator,Text } from "react-native";
-import {connect} from 'react-redux'
-import {bindActionCreators} from 'redux'
-import * as RepositoriesActions from '../../store/actions/repositories'
-import { Container } from './styles';
-
+import { ActivityIndicator, Text } from "react-native";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as RepositoriesActions from "../../store/actions/repositories";
+import { Container } from "./styles";
+import Login from "~/pages/Login";
 
 class Repositories extends Component {
-  componentDidMount(){
-    const {loadRepositoriesRequest} = this.props
-    loadRepositoriesRequest()
+  componentDidMount() {
+    const { loadRepositoriesRequest } = this.props;
+    loadRepositoriesRequest();
   }
   render() {
-    const {repositories}=this.props
-    console.tron.log(repositories.data)
-    return (<Container>
-      {repositories.loading?<ActivityIndicator size="small" color="#999"/>:repositories.data.map(repository=><Text key={"repository.id"}>{repository.name}</Text>)
-      }
-    </Container>
-    )}
+    const { repositories } = this.props;
+    console.tron.log(repositories.data);
+    return (
+      <Container>
+        {repositories.loading ? (
+          <ActivityIndicator size="small" color="#999" />
+        ) : (
+          repositories.data.map(repository => (
+            <Text key={"repository.id"}>{repository.name}</Text>
+          ))
+        )}
+      </Container>
+    );
+  }
 }
 const mapStateToProps = state => ({
-  repositories:state.repositories
+  repositories: state.repositories,
 });
-const mapDispatchToProps = (dispatch) =>
-  bindActionCreators(RepositoriesActions,dispatch)
-export default connect(mapStateToProps,mapDispatchToProps)(Repositories)
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(RepositoriesActions, dispatch);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Repositories);
